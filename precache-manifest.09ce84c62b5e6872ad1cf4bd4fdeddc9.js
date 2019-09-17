60363f89376cd292ad09a6ac5e35cfd095219803
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-09-17 00:17:04 -0400

    Updates

---
+++ precache-manifest.09ce84c62b5e6872ad1cf4bd4fdeddc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0cb477649134c14a71429ad25d75d9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e5c2c24db1ad788d8ee",
+    "url": "/static/css/main.148163f9.chunk.css"
+  },
+  {
+    "revision": "8fe0b566f27462b4b6ee",
+    "url": "/static/js/2.f4dd1851.chunk.js"
+  },
+  {
+    "revision": "2e5c2c24db1ad788d8ee",
+    "url": "/static/js/main.f282c207.chunk.js"
+  },
+  {
+    "revision": "416cc1c79b8c22940a8c",
+    "url": "/static/js/runtime~main.7302273d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/daniela-sandoval.com/precache-manifest.03b5ba6f24969efda13b9d800944c86f.js"
+  "/precache-manifest.09ce84c62b5e6872ad1cf4bd4fdeddc9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/daniela-sandoval.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
