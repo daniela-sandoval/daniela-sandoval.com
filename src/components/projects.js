dce3b4de81b36b16d631a8be380a9d3cc1d48daa
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-10-20 05:40:05 -0400

    clean single project

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import { ReactComponent as Logo } from '../img/logo1.svg'
 import styled from 'styled-components'
-import FloriographyCarousel from './project-slideshow/floriography-pics.js'
-import FlorPic1 from '../img/flor-pic1.png'
+import FlorPic1 from '../img/florpic2.png'
 import '../Stylesheets/projects.scss'
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 
 const StyledLogo = styled(Logo) `
   position: fixed
-  height: 6vw;
+  height: 5vw;
   right: 36px;
   top: 10px;
   z-index: 10;
@@ -25,53 +23,18 @@ class Projects extends Component {
         <h1>projects</h1>
         <div className="project-list">
           <div className="single-project">
-            <div className="project-pics">
-              <FloriographyCarousel/>
-            </div>
+            <div className="project-pics" style={{backgroundImage: `url(${FlorPic1})`}}></div>
             <div className="information">
-              <h2>Title</h2>
-              <h4>info like date</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              <p>Tech Stack: abc, abc, abc, abc</p>
+              <h2>Floriography</h2>
+              <h4>August 2019</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <p>Tech: Rails, ActionMailer, React, Redux, Watson Tone Analyzer</p>
               <hr></hr>
               <div className="btns">
-                <button>code</button>
-                <button>live</button>
-                <button>demo</button>
-              </div>
-            </div>
-          </div>
-          <div className="single-project">
-            <div className="project-pics">
-              <FloriographyCarousel/>
-            </div>
-            <div className="information">
-              <h2>Title</h2>
-              <h4>info like date</h4>
-              <p>asdlkajsdlkajsdlkajsdlakjsdlaksjdlakjsdlakjsdlaksjdlkasjd</p>
-              <p>Tech Stack: abc, abc, abc, abc</p>
-              <hr></hr>
-              <div className="btns">
-                <button>code</button>
-                <button>live</button>
-                <button>demo</button>
-              </div>
-            </div>
-          </div>
-          <div className="single-project">
-            <div className="project-pics">
-              <FloriographyCarousel/>
-            </div>
-            <div className="information">
-              <h2>Title</h2>
-              <h4>info like date</h4>
-              <p>asdlkajsdlkajsdlkajsdlakjsdlaksjdlakjsdlakjsdlaksjdlkasjd</p>
-              <p>Tech Stack: abc, abc, abc, abc</p>
-              <hr></hr>
-              <div className="btns">
-                <button>code</button>
-                <button>live</button>
-                <button>demo</button>
+                <a href="https://github.com/daniela-sandoval/Floriography-App-Frontend" target="_blank" rel="noopener noreferrer"><button>front</button></a>
+                <a href="https://github.com/daniela-sandoval/Floriography-App-Backend" target="_blank" rel="noopener noreferrer"><button>back</button></a>
+                <a href="https://floriography.herokuapp.com" target="_blank" rel="noopener noreferrer"><button>live</button></a>
+                <a href="https://youtu.be/3J0I3uoayso" target="_blank" rel="noopener noreferrer"><button>demo</button></a>
               </div>
             </div>
           </div>
@@ -81,20 +44,3 @@ class Projects extends Component {
   }
 }
 export default Projects;
-
-// <div className="single-project">
-//   <div className="project-pics" style={{backgroundImage: `url(${FlorPic1})`}}></div>
-//   <div className="information">
-//     <h2>Title</h2>
-//     <h4>info like date</h4>
-//     <p>asdlkajsdlkajsdlkajsdlakjsdlaksjdlakjsdlakjsdlaksjdlkasjd</p>
-//     <p>Tech Stack: abc, abc, abc, abc</p>
-//     <hr></hr>
-//     <div className="btns">
-//       <button>code</button>
-//       <button>live</button>
-//       <button>demo</button>
-//     </div>
-//   </div>
-// </div>
-//
