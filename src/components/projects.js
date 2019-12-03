6e7e414347ef6c3c49f1f3fea0f24f93349bfa3c
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-12-03 15:19:38 -0500

    adds mobile responsiveness to project section

--- src/components/projects.js
+++ src/components/projects.js
@@ -39,11 +39,11 @@ class Projects extends Component {
     return (
       <div className="project-container">
         <a href="/">
-          <StyledLogo class="logo"/>
+          <StyledLogo className="logo"/>
         </a>
         <div className="project-visual">
           <h1>projects</h1>
-          <StyledLaptop />
+          <StyledLaptop className="svg-project"/>
         </div>
         <div className="project-list">
           <div className="single-project">
@@ -105,49 +105,3 @@ class Projects extends Component {
   }
 }
 export default Projects;
-
-
-// <div className="project-container">
-//   <a href="/">
-//     <StyledLogo class="logo"/>
-//   </a>
-//   <h1>projects</h1>
-//   <div className="project-list">
-//     <div class="column_one">
-//       <div className="single-project">
-//         <div className="project-pics" style={{backgroundImage: `url(${FlorPic1})`}}></div>
-//         <div className="information">
-//           <h2>Floriography</h2>
-//           <h4>August 2019</h4>
-//           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-//           <p>Tech: Rails, ActionMailer, React, Redux, Watson Tone Analyzer</p>
-//           <hr></hr>
-//           <div className="btns">
-//             <a href="https://github.com/daniela-sandoval/Floriography-App-Frontend" target="_blank" rel="noopener noreferrer"><button>front</button></a>
-//             <a href="https://github.com/daniela-sandoval/Floriography-App-Backend" target="_blank" rel="noopener noreferrer"><button>back</button></a>
-//             <a href="https://floriography.herokuapp.com" target="_blank" rel="noopener noreferrer"><button>live</button></a>
-//             <a href="https://youtu.be/3J0I3uoayso" target="_blank" rel="noopener noreferrer"><button>demo</button></a>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//     <div class="column_two">
-//       <div className="single-project">
-//         <div className="project-pics" style={{backgroundImage: `url(${FlorPic1})`}}></div>
-//         <div className="information">
-//           <h2>Floriography</h2>
-//           <h4>August 2019</h4>
-//           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-//           <p>Tech: Rails, ActionMailer, React, Redux, Watson Tone Analyzer</p>
-//           <hr></hr>
-//           <div className="btns">
-//             <a href="https://github.com/daniela-sandoval/Floriography-App-Frontend" target="_blank" rel="noopener noreferrer"><button>front</button></a>
-//             <a href="https://github.com/daniela-sandoval/Floriography-App-Backend" target="_blank" rel="noopener noreferrer"><button>back</button></a>
-//             <a href="https://floriography.herokuapp.com" target="_blank" rel="noopener noreferrer"><button>live</button></a>
-//             <a href="https://youtu.be/3J0I3uoayso" target="_blank" rel="noopener noreferrer"><button>demo</button></a>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-// </div>
